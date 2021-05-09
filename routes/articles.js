7acbc214092c0dcac9fd6d4bd0d2862559522ac0
amarh-ayman <amarhbishtawi@gmail.com>
2021-05-09 23:52:26 +0300

    finish the admin rendering for the articles

--- routes/articles.js
+++ routes/articles.js
@@ -1,13 +1,35 @@
-const express = require("express");
+'use strict';
 
+const express = require('express');
+const pg = require('pg');
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  // ssl: { rejectUnauthorized: false },
+});
 const router = express.Router();
 
 // Put your routes here
 
 // /articles
-router.get("/", function (req, res) {
-  res.render("pages/articles/articles", { title: "articles" });
+router.get('/', function (req, res) {
+  res.render('pages/articles/articles', { title: 'articles' });
 });
+
+// router.get('/admin', function (req, res) {
+//   let SQL = `SELECT * FROM articles`;
+//   console.log('dddd');
+//   client
+//     .query(SQL)
+//     .then(result => {
+//       console.log(result);
+//       res.render('pages/articles/admin', { ArticlesData: result.row[0] });
+//     })
+//     .catch(err => {
+//       res.render('pages/error', { error: err });
+//     });
+// });
+
+//function Articles(data_Artc) {}
 
 // export router;
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 // Application Dependencies
-require("dotenv").config();
-const express = require("express");
-const superagent = require("superagent");
-const pg = require("pg");
-const methodOverride = require("method-override");
+require('dotenv').config();
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const methodOverride = require('method-override');
 
 // Application Setups
 const PORT = process.env.PORT || 3030;
@@ -15,41 +15,111 @@ const client = new pg.Client({
   // ssl: { rejectUnauthorized: false },
 });
 server.use(express.urlencoded({ extended: true }));
-server.set("view engine", "ejs");
-server.use(express.static("./public"));
-server.use(methodOverride("_method"));
+server.set('view engine', 'ejs');
+server.use(express.static('./public'));
+server.use(methodOverride('_method'));
 
 // Start you routes here
 
 // Articles route
-const articlesRoute = require("./routes/articles.js");
-server.use("/articles", articlesRoute);
+const articlesRoute = require('./routes/articles.js');
+server.use('/articles', articlesRoute);
+
+server.get('/articles/admin', (req, res) => {
+  let SQL = `SELECT * FROM articles`;
+  client
+    .query(SQL)
+    .then(result => {
+      // console.log(result.rows);
+      res.render('pages/articles/admin', { articlesData: result.rows });
+    })
+    .catch(err => {
+      res.render('pages/error', { error: err });
+    });
+});
+
+server.post('/articles/admin', (req, res) => {
+  let { image, date, title, author, description } = req.body;
+  console.log(req.body);
+  let SQL = `INSERT INTO articlesforcustmor (image, date, title, author, description) VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
+
+  let safeValues = [image, date, title, author, description];
+
+  client
+    .query(SQL, safeValues)
+    .then(res.redirect('/articles/admin'))
+    .catch(err => {
+      res.render('pages/error', { error: err });
+    });
+});
+server.put('/articles/admin', (req, res) => {
+  let { image, date, title, author, description, id } = req.body;
+  // console.log(id);
+  let SQL = `UPDATE articles SET image=$1, date=$2, title=$3, author=$4, description=$5 WHERE id=$6;`;
+  let safeValues = [image, date, title, author, description, id];
+  // console.log(safeValues);
+  client
+    .query(SQL, safeValues)
+    .then(r => {
+      // console.log(r);
+      res.redirect('/articles/admin');
+    })
+    .catch(err => {
+      res.render('pages/error', { error: err });
+    });
+});
+
+server.delete('/articles/admin', (req, res) => {
+  let { id } = req.body;
+  console.log(id);
+  let SQL = `DELETE FROM articlesforcustmor WHERE id=$1;`;
+
+  client
+    .query(SQL, [id])
+    .then(res.redirect('/articles/articles'))
+    .catch(err => {
+      res.render('pages/error', { error: err });
+    });
+});
+
+server.get('/articles/articles', (req, res) => {
+  let SQL = `SELECT * FROM articlesforcustmor`;
+  client
+    .query(SQL)
+    .then(result => {
+      // console.log(result.rows);
+      res.render('pages/articles/articles', { articlesData: result.rows });
+    })
+    .catch(err => {
+      res.render('pages/error', { error: err });
+    });
+});
 
 // results route ------- most IMPORTANT route ---- APIs here
-server.get("/results/:search_query", (req, res) => {
+server.get('/results/:search_query', (req, res) => {
   let URLparams = [req.params.search_query]; //Will be used in APIs functions as a search query
   // APIs functions here
 
   // retreive reviews from DB
 
   // render data on results/:search_query { title: "results", API1: "results",API2: "results",API3: "results",.......,Data from DB: "results", }
-  res.render("pages/results", { title: "results" });
+  res.render('pages/results', { title: 'results' });
 });
 
 // To insert reviews into the DB
-server.post("/results/search_query", (req, res) => {
+server.post('/results/search_query', (req, res) => {
   let URLparams = [req.params.search_query];
   res.redirect(`/results/${URLparams}`);
 });
 
 // HomePage route
-server.get("/", (req, res) => {
-  res.render("pages/index", { title: "HomePage" });
+server.get('/', (req, res) => {
+  res.render('pages/index', { title: 'HomePage' });
 });
 
 // For any route that is not specified
-server.get("*", (req, res) => {
-  res.render("pages/error");
+server.get('*', (req, res) => {
+  res.render('pages/error');
 });
 
 //Connect to dataBase then listen to the PORT
