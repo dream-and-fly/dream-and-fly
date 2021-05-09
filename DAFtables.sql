e719fbda3e03e51e0a27bdb4142280c93374bf27
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-05-09 16:19:08 +0300

    for nimrawi

---
+++ DAFtables.sql
@@ -0,0 +1,72 @@
+DROP TABLE IF EXISTS articles;
+CREATE TABLE articles (
+  id SERIAL PRIMARY KEY,
+  date VARCHAR(255),
+  title VARCHAR(255),
+  author VARCHAR(255),
+  image VARCHAR(255),
+  description TEXT
+);
+
+INSERT INTO articles (date, title, author, image, description) 
+VALUES('25/04/2021','Green bailouts: relying on carbon offsetting will let polluting airlines off the hook','Ben Christopher Howard','https://images.theconversation.com/files/337957/original/file-20200527-20229-u4lup3.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=503&fit=crop&dpr=1','The coronavirus pandemic has grounded thousands of aircraft, contributing to the largest-ever annual fall in CO₂ emissions. At some point though, the planes will soar again and with them, global emissions.
+
+Most airlines in the UK have committed to achieving net zero carbon emissions by 2050. From 2026, it will become mandatory for airlines worldwide to ensure that their annual emissions stay flat. But the UK aviation industry also plans to increase the number of passengers it serves by 70% in the next three decades.
+
+To pull this off, airlines will be planning to fly planes at or near full passenger capacity and use cleaner burning fuels. But the rest of the emissions airlines hope to cut – between one-third and half of the total – are expected to come from market-based measures, such as carbon offsetting and emissions trading.
+
+You’ve probably encountered an option to offset your carbon footprint when buying a flight. The payment page of Ryanair’s website suggested a “carbon offset contribution” of £2 for a return flight from Gatwick to Alicante. Airlines seeking government bailouts are likely to use carbon offsetting and emissions trading to show they’re serious about putting their emissions on a downward trajectory. But what do they involve and are they really a solution to climate change?');
+
+INSERT INTO articles (date, title, author, image, description) 
+VALUES('27/04/2021','Grounded aircraft could make weather forecasts less reliable','Matthew Blackett','https://images.theconversation.com/files/334309/original/file-20200512-175224-10qscu0.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=525&fit=crop&dpr=1','The coronavirus pandemic has grounded thousands of aircraft, contributing to the largest-ever annual fall in CO₂ emissions. At some point though, the planes will soar again and with them, global emissions.
+
+Most airlines in the UK have committed to achieving net zero carbon emissions by 2050. From 2026, it will become mandatory for airlines worldwide to ensure that their annual emissions stay flat. But the UK aviation industry also plans to increase the number of passengers it serves by 70% in the next three decades.
+
+To pull this off, airlines will be planning to fly planes at or near full passenger capacity and use cleaner burning fuels. But the rest of the emissions airlines hope to cut – between one-third and half of the total – are expected to come from market-based measures, such as carbon offsetting and emissions trading.
+
+You’ve probably encountered an option to offset your carbon footprint when buying a flight. The payment page of Ryanair’s website suggested a “carbon offset contribution” of £2 for a return flight from Gatwick to Alicante. Airlines seeking government bailouts are likely to use carbon offsetting and emissions trading to show they’re serious about putting their emissions on a downward trajectory. But what do they involve and are they really a solution to climate change?');
+
+INSERT INTO articles (date, title, author, image, description) 
+VALUES('22/04/2021','Plane, train, or automobile? The climate impact of transport is surprisingly complicated','Laurie Wright','https://images.theconversation.com/files/315513/original/file-20200214-11011-8hr434.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=499&fit=crop&dpr=1','The coronavirus pandemic has grounded thousands of aircraft, contributing to the largest-ever annual fall in CO₂ emissions. At some point though, the planes will soar again and with them, global emissions.
+
+Most airlines in the UK have committed to achieving net zero carbon emissions by 2050. From 2026, it will become mandatory for airlines worldwide to ensure that their annual emissions stay flat. But the UK aviation industry also plans to increase the number of passengers it serves by 70% in the next three decades.
+
+To pull this off, airlines will be planning to fly planes at or near full passenger capacity and use cleaner burning fuels. But the rest of the emissions airlines hope to cut – between one-third and half of the total – are expected to come from market-based measures, such as carbon offsetting and emissions trading.
+
+You’ve probably encountered an option to offset your carbon footprint when buying a flight. The payment page of Ryanair’s website suggested a “carbon offset contribution” of £2 for a return flight from Gatwick to Alicante. Airlines seeking government bailouts are likely to use carbon offsetting and emissions trading to show they’re serious about putting their emissions on a downward trajectory. But what do they involve and are they really a solution to climate change?');
+
+INSERT INTO articles (date, title, author, image, description) 
+VALUES('February 14, 2020 2.23am AEDT','Climate change means longer take-offs and fewer passengers per aeroplane – new study','Guy Gratton','https://images.theconversation.com/files/315079/original/file-20200212-61935-1l0sexc.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=503&fit=crop&dpr=1','The coronavirus pandemic has grounded thousands of aircraft, contributing to the largest-ever annual fall in CO₂ emissions. At some point though, the planes will soar again and with them, global emissions.
+
+Most airlines in the UK have committed to achieving net zero carbon emissions by 2050. From 2026, it will become mandatory for airlines worldwide to ensure that their annual emissions stay flat. But the UK aviation industry also plans to increase the number of passengers it serves by 70% in the next three decades.
+
+To pull this off, airlines will be planning to fly planes at or near full passenger capacity and use cleaner burning fuels. But the rest of the emissions airlines hope to cut – between one-third and half of the total – are expected to come from market-based measures, such as carbon offsetting and emissions trading.
+
+You’ve probably encountered an option to offset your carbon footprint when buying a flight. The payment page of Ryanair’s website suggested a “carbon offset contribution” of £2 for a return flight from Gatwick to Alicante. Airlines seeking government bailouts are likely to use carbon offsetting and emissions trading to show they’re serious about putting their emissions on a downward trajectory. But what do they involve and are they really a solution to climate change?');
+
+INSERT INTO articles (date, title, author, image, description) 
+VALUES('October 25, 2019 9.23pm AEDT ','Flight shame: flying less plays a small but positive part in tackling climate change','Jan Ditzen','https://images.theconversation.com/files/298187/original/file-20191022-55679-18c810p.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=391&fit=crop&dpr=1','The coronavirus pandemic has grounded thousands of aircraft, contributing to the largest-ever annual fall in CO₂ emissions. At some point though, the planes will soar again and with them, global emissions.
+
+Most airlines in the UK have committed to achieving net zero carbon emissions by 2050. From 2026, it will become mandatory for airlines worldwide to ensure that their annual emissions stay flat. But the UK aviation industry also plans to increase the number of passengers it serves by 70% in the next three decades.
+
+To pull this off, airlines will be planning to fly planes at or near full passenger capacity and use cleaner burning fuels. But the rest of the emissions airlines hope to cut – between one-third and half of the total – are expected to come from market-based measures, such as carbon offsetting and emissions trading.
+
+You’ve probably encountered an option to offset your carbon footprint when buying a flight. The payment page of Ryanair’s website suggested a “carbon offset contribution” of £2 for a return flight from Gatwick to Alicante. Airlines seeking government bailouts are likely to use carbon offsetting and emissions trading to show they’re serious about putting their emissions on a downward trajectory. But what do they involve and are they really a solution to climate change?');
+
+INSERT INTO articles (date, title, author, image, description) 
+VALUES('August 28, 2019 9.48pm AEST','Why would anyone want to sit on a plane for over 18 hours? An economist takes the world’s longest flight','Jay L. Zagorsky','https://images.theconversation.com/files/289516/original/file-20190826-8856-1rxgxj3.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=565&fit=crop&dpr=1','The coronavirus pandemic has grounded thousands of aircraft, contributing to the largest-ever annual fall in CO₂ emissions. At some point though, the planes will soar again and with them, global emissions.
+
+Most airlines in the UK have committed to achieving net zero carbon emissions by 2050. From 2026, it will become mandatory for airlines worldwide to ensure that their annual emissions stay flat. But the UK aviation industry also plans to increase the number of passengers it serves by 70% in the next three decades.
+
+To pull this off, airlines will be planning to fly planes at or near full passenger capacity and use cleaner burning fuels. But the rest of the emissions airlines hope to cut – between one-third and half of the total – are expected to come from market-based measures, such as carbon offsetting and emissions trading.
+
+You’ve probably encountered an option to offset your carbon footprint when buying a flight. The payment page of Ryanair’s website suggested a “carbon offset contribution” of £2 for a return flight from Gatwick to Alicante. Airlines seeking government bailouts are likely to use carbon offsetting and emissions trading to show they’re serious about putting their emissions on a downward trajectory. But what do they involve and are they really a solution to climate change?');
+
+INSERT INTO articles (date, title, author, image, description) 
+VALUES('August 23, 2019 2.16am AEST','Travel the world without destroying it – Imagine newsletter #5','Ben Christopher Howard','https://images.theconversation.com/files/288029/original/file-20190814-136213-oahfzr.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=503&fit=crop&dpr=1','The coronavirus pandemic has grounded thousands of aircraft, contributing to the largest-ever annual fall in CO₂ emissions. At some point though, the planes will soar again and with them, global emissions.
+
+Most airlines in the UK have committed to achieving net zero carbon emissions by 2050. From 2026, it will become mandatory for airlines worldwide to ensure that their annual emissions stay flat. But the UK aviation industry also plans to increase the number of passengers it serves by 70% in the next three decades.
+
+To pull this off, airlines will be planning to fly planes at or near full passenger capacity and use cleaner burning fuels. But the rest of the emissions airlines hope to cut – between one-third and half of the total – are expected to come from market-based measures, such as carbon offsetting and emissions trading.
+
+You’ve probably encountered an option to offset your carbon footprint when buying a flight. The payment page of Ryanair’s website suggested a “carbon offset contribution” of £2 for a return flight from Gatwick to Alicante. Airlines seeking government bailouts are likely to use carbon offsetting and emissions trading to show they’re serious about putting their emissions on a downward trajectory. But what do they involve and are they really a solution to climate change?');
