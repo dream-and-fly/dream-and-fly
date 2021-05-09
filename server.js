a5c82e35eb222ee5de811077ff48c972f464f9d2
tareqmeg <tareq.megdadi@gmail.com>
2021-05-09 14:09:36 +0300

    Add youtube api

--- server.js
+++ server.js
@@ -29,11 +29,34 @@ server.use("/articles", articlesRoute);
 server.get("/results/:search_query", (req, res) => {
   let URLparams = [req.params.search_query]; //Will be used in APIs functions as a search query
   // APIs functions here
+function youTubeApi(URLparams) {
+  let newcityData;
+  function City (serchQuery, items){
+    this.cityName= serchQuery;
+    this.videoID = items.id.videoId;
+    this.videotitle = items.snippet.title;
+    this.cityVideo = items;
+  }
+  // let serchQuery =req.query.search;
+  let key= process.env.YOUTUBE_API_KEY;
+  let url =`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=tourism+in+${URLparams}&type=video&videoCaption=any&videoDefinition=any&videoDimension=2d&videoDuration=medium&videoEmbeddable=true&videoType=any&key=${key}`;
+  superagent.get(url).then(cityData=>{
+    console.log(cityData.body.volumeInfo);
+    let gettedData = cityData.body.items;
+    newcityData = gettedData.map((items)=>{
+      return new City(URLparams, items);
+    });
+    // res.send(newcityData);
+    // res.render('video', {video:newcityData});
+    // res.render('index', {video:newcityData});
+  });
+  return newcityData;
 
+}
   // retreive reviews from DB
 
   // render data on results/:search_query { title: "results", API1: "results",API2: "results",API3: "results",.......,Data from DB: "results", }
-  res.render("pages/results", { title: "results" });
+  res.render("pages/results", { title: "results", youtube:youTubeApi(URLparams) });
 });
 
 // To insert reviews into the DB
