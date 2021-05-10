d94827ebbca0744f0e8a8a0a18fb35158b42f6c6
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-05-10 12:06:33 +0300

    psuh the homepage

---
+++ project-home-header/app.js
@@ -0,0 +1,10 @@
+$('#updateForm').hide();
+$('#updateBtn').on('click',function(){
+  $('#updateForm').toggle()
+});
+
+window.onclick = function(event) {
+    if (!event.target.matches('#updateBtn')) {
+        $('#updateForm').hide();
+    }
+  }
