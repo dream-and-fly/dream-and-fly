8b1450a3fcbb6724b886a1ac49a093f7ffec766f
amarh-ayman <amarhbishtawi@gmail.com>
2021-05-14 22:55:56 +0300

    added new articles by admin

--- routes/articles.js
+++ routes/articles.js
@@ -69,13 +69,21 @@ router.put('/admin', (req, res) => {
 
 router.delete('/admin', (req, res) => {
   let { deleteN } = req.body;
-
-  let SQL =
-    deleteN === 'All'
+  let { Delete_root_articles_id } = req.body;
+  console.log(deleteN);
+  console.log(Delete_root_articles_id);
+  let SQL = deleteN
+    ? deleteN === 'All'
       ? client.query(`DELETE FROM articlesforcustmor ;`)
-      : client.query(`DELETE FROM articlesforcustmor WHERE id=$1;`, [deleteN]);
+      : client.query(`DELETE FROM articlesforcustmor WHERE id=$1;`, [deleteN])
+    : client.query(`DELETE FROM articles WHERE id=$1;`, [
+        Delete_root_articles_id,
+      ]);
 
-  SQL.then(res.redirect('/articles/admin'));
+  SQL.then(() => {
+    // console.log(SQL);
+    res.redirect('/articles/admin');
+  });
 });
 
 router.get('/article/:id', (req, res) => {
@@ -91,6 +99,52 @@ router.get('/article/:id', (req, res) => {
     .catch(err => {
       res.render('pages/error', { error: err });
     });
+});
+
+router.get('/new-post', (req, res) => {
+  let SQL = `SELECT * FROM articles ORDER BY id DESC LIMIT 3;`;
+  client.query(SQL).then(result => {
+    res.render('pages/articles/newPost', {
+      newArticlesAddes: result.rows,
+      title: 'new Post',
+    });
+  });
+});
+
+router.post('/new-post', (req, res) => {
+  let { image, title, author, description } = req.body;
+  // let date = new Date();
+  let date = new Intl.DateTimeFormat('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }).format(new Date());
+
+  let SQL = `INSERT INTO articles (date ,image, title, author, description) VALUES ($1,$2,$3,$4,$5) RETURNING *; `;
+  let safeValues = [date, image, title, author, description];
+
+  client.query(SQL, safeValues).then(result => {
+    res.redirect('/articles/new-post');
+  });
+});
+
+router.put('/new-post', (req, res) => {
+  let { image, date, title, author, description, id } = req.body;
+  let SQL = `UPDATE articles SET image=$1, date=$2, title=$3, author=$4, description=$5 WHERE id=$6;`;
+  let safeValues = [image, date, title, author, description, id];
+
+  client
+    .query(SQL, safeValues)
+    .then(res.redirect('/articles/new-post'))
+    .catch(err => {
+      res.render('pages/error', { error: err });
+    });
+});
+
+router.delete('/new-post', (req, res) => {
+  let SQL = `DELETE FROM articles WHERE id =$1;`;
+  let safeValues = [req.body.deleteN];
+  client.query(SQL, safeValues).then(res.redirect('/articles//new-post'));
 });
 
 module.exports = router;
