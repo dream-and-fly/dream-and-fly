f709f18cd65e0945dd417e9a272f5ef3f48b4c21
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-05-16 02:04:45 +0300

    Finish the resutls section

--- server.js
+++ server.js
@@ -48,9 +48,42 @@ function CountryInfo(items) {
   this.language = items[0].languages[0].name;
   this.currencies = items[0].currencies[0].name;
 }
+function Covid(items) {
+  this.confirmed = items.confirmed;
+  this.recovered = items.recovered;
+  this.deaths = items.deaths;
+}
 // results route ------- most IMPORTANT route ---- APIs here
-server.get("/results/:search_query", (req, res) => {
-  let URLparams = req.params.search_query;
+server.get("/results", (req, res) => {
+  // console.log("sdf");
+  let URLparams = req.query.search_query;
+  // console.log(URLparams);
+  function countryNameFromCity(URLparams) {
+    let key = process.env.GET_COUNTRY_API;
+    let url = `http://api.positionstack.com/v1/forward?access_key=${key}&query=${URLparams}`;
+    return superagent.get(url).then((countryData) => {
+      let gettedData = countryData.body.data[0].country;
+      return gettedData;
+    });
+  }
+  let countryApi = countryNameFromCity(URLparams).then((results) => {
+    let url = `https://restcountries.eu/rest/v2/name/${results}?fullText=true`;
+    return superagent.get(url).then((capitalData) => {
+      let gettedData = capitalData.body;
+      let newcapitalData = new CountryInfo(gettedData);
+      return newcapitalData;
+    });
+  });
+  let covidApi = countryNameFromCity(URLparams).then((results) => {
+    let url = `https://covid-api.mmediagroup.fr/v1/cases?country=${
+      results[0].toUpperCase() + results.slice(1)
+    }`;
+    return superagent.get(url).then((covidData) => {
+      let Data = covidData.body.All;
+      let CovidObjects = new Covid(Data);
+      return CovidObjects;
+    });
+  });
   function youTubeApi(URLparams) {
     let key = process.env.YOUTUBE_API_KEY;
     let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=tourism+in+${URLparams}&type=video&videoCaption=any&videoDefinition=any&videoDimension=2d&videoDuration=medium&videoEmbeddable=true&videoType=any&key=${key}`;
@@ -63,45 +96,68 @@ server.get("/results/:search_query", (req, res) => {
       return threeVideos;
     });
   }
-  function weatherData(URLparams) {
+  function weatherApi(URLparams) {
     let key = process.env.WEATHER_API_KEY;
     let url = `api.openweathermap.org/data/2.5/weather?q=${URLparams}&units=metric&appid=${key}`;
     return superagent.get(url).then((wData) => {
       let gettedData = wData.body;
-      console.log(gettedData);
+      // console.log(gettedData);
       let newweatherData = new Weather(gettedData);
       return newweatherData;
     });
   }
-  function countryData(URLparams) {
-    let key = process.env.GET_COUNTRY_API;
-    let url = `http://api.positionstack.com/v1/forward?access_key=${key}&query=${URLparams}`;
-    return superagent.get(url).then((countryData) => {
-      let gettedData = countryData.body.data[0].country;
-      let url = `https://restcountries.eu/rest/v2/name/${gettedData}?fullText=true`;
-      return superagent.get(url).then((capitalData) => {
-        let gettedData = capitalData.body;
-        let newcapitalData = new CountryInfo(gettedData);
-        return newcapitalData;
-      });
+  function getRevies(URLparams) {
+    let safeValues = [URLparams.toLowerCase()];
+    let SQL = `SELECT * FROM reviews WHERE city=$1;`;
+
+    return client.query(SQL, safeValues).then((results) => {
+      // console.log(results.rows);
+      // res.render("pages/results", { title: "sds", DBresults: results.rows });
+      return results.rows;
     });
   }
-
   Promise.all([
     youTubeApi(URLparams),
-    weatherData(URLparams),
-    countryData(URLparams),
-  ]).then((results) => {
-    // console.log(results);
-    res.send(results);
-    // res.render("pages/results", { title: "results" });
-  });
+    weatherApi(URLparams),
+    countryApi,
+    covidApi,
+    getRevies(URLparams),
+  ])
+    .then((results) => {
+      console.log(results[4]);
+      res.render("pages/results", {
+        title: "results",
+        youTubeApi: results[0],
+        weatherApi: results[1],
+        countryApi: results[2],
+        covidApi: results[3],
+        DBresults: results[4],
+        city: URLparams,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send(error);
+      // res.render("./pages/error", { error: error });
+    });
 });
 
 // To insert reviews into the DB
-server.post("/results/search_query", (req, res) => {
-  let URLparams = [req.params.search_query];
-  res.redirect(`/results/${URLparams}`);
+server.post("/results", (req, res) => {
+  // let URLparams = [req.params.search_query];
+  console.log(req.body);
+  let SQL = `INSERT INTO reviews (city, date, title, author, description,rating) VALUES($1,$2,$3,$4,$5,$6); `;
+  let safeValues = [
+    req.body.city,
+    req.body.date,
+    req.body.title,
+    req.body.author,
+    req.body.description,
+    req.body.rating,
+  ];
+  client.query(SQL, safeValues).then((results) => {
+    res.redirect(`/results/?search_query=${req.body.city}`);
+  });
 });
 
 // HomePage route
@@ -114,12 +170,12 @@ server.get("*", (req, res) => {
   res.render("pages/error", { title: "Error" });
 });
 
-//Connect to dataBase then listen to the PORT
-// client.connect().then(() => {
-//   server.listen(PORT, () =>
-//     console.log(`Server running on port: http://localhost:${PORT}`)
-//   );
-// });
-server.listen(PORT, () =>
-  console.log(`Server running on port: http://localhost:${PORT}`)
-);
+// Connect to dataBase then listen to the PORT
+client.connect().then(() => {
+  server.listen(PORT, () =>
+    console.log(`Server running on port: http://localhost:${PORT}`)
+  );
+});
+// server.listen(PORT, () =>
+//   console.log(`Server running on port: http://localhost:${PORT}`)
+// );
