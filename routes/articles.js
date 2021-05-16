4af36fb7b49b7f7247adc91a7433e4db0b572eef
amarh-ayman <amarhbishtawi@gmail.com>
2021-05-16 13:45:41 +0300

    need admin and pass login

--- routes/articles.js
+++ routes/articles.js
@@ -1,46 +1,46 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
-const client = require("../server.js");
+const client = require('../server.js');
 
 // Put your routes here
 
 // /articles
-router.get("/", function (req, res) {
+router.get('/', function (req, res) {
   // Show all the articles from the DB
   let SQL = `select * from articles inner join articlesforcustmor on articles.id=articlesforcustmor.id;`;
   client
     .query(SQL)
-    .then((result) => {
-      res.render("pages/articles/articles", {
+    .then(result => {
+      res.render('pages/articles/articles', {
         articlesDataforCustomer: result.rows,
-        title: "Articles",
+        title: 'Articles',
       });
     })
-    .catch((err) => {
-      res.render("pages/error", { error: err });
+    .catch(err => {
+      res.render('pages/error', { error: err });
     });
 });
 
-router.get("/admin", (req, res) => {
+router.get('/admin', (req, res) => {
   let SQL = `SELECT * FROM articles; select * from articles inner join articlesforcustmor on articles.id=articlesforcustmor.id; `;
   client
     .query(SQL)
-    .then((result) => {
-      res.render("pages/articles/admin", {
+    .then(result => {
+      res.render('pages/articles/admin', {
         articlesData: result[0].rows,
         addedDataCheck: result[1].rows,
-        title: "Admin",
+        title: 'Admin',
       });
     })
-    .catch((err) => {
-      res.render("pages/error", { error: err, title: "results" });
+    .catch(err => {
+      res.render('pages/error', { error: err, title: 'results' });
     });
 });
 
-router.post("/admin", (req, res) => {
+router.post('/admin', (req, res) => {
   let { id } = req.body;
   let SQL = `INSERT INTO articlesforcustmor (id) VALUES ($1);`;
 
@@ -48,32 +48,32 @@ router.post("/admin", (req, res) => {
 
   client
     .query(SQL, safeValues)
-    .then(res.redirect("/articles/admin"))
-    .catch((err) => {
-      res.render("pages/error", { error: err });
+    .then(res.redirect('/articles/admin'))
+    .catch(err => {
+      res.render('pages/error', { error: err });
     });
 });
 
-router.put("/admin", (req, res) => {
+router.put('/admin', (req, res) => {
   let { image, date, title, author, description, id } = req.body;
   let SQL = `UPDATE articles SET image=$1, date=$2, title=$3, author=$4, description=$5 WHERE id=$6;`;
   let safeValues = [image, date, title, author, description, id];
 
   client
     .query(SQL, safeValues)
-    .then(res.redirect("/articles/admin"))
-    .catch((err) => {
-      res.render("pages/error", { error: err });
+    .then(res.redirect('/articles/admin'))
+    .catch(err => {
+      res.render('pages/error', { error: err });
     });
 });
 
-router.delete("/admin", (req, res) => {
+router.delete('/admin', (req, res) => {
   let { deleteN } = req.body;
   let { Delete_root_articles_id } = req.body;
   console.log(deleteN);
   console.log(Delete_root_articles_id);
   let SQL = deleteN
-    ? deleteN === "All"
+    ? deleteN === 'All'
       ? client.query(`DELETE FROM articlesforcustmor ;`)
       : client.query(`DELETE FROM articlesforcustmor WHERE id=$1;`, [deleteN])
     : client.query(`DELETE FROM articles WHERE id=$1;`, [
@@ -82,69 +82,89 @@ router.delete("/admin", (req, res) => {
 
   SQL.then(() => {
     // console.log(SQL);
-    res.redirect("/articles/admin");
+    res.redirect('/articles/admin');
+  }).catch(err => {
+    res.render('pages/error', { error: err });
   });
 });
 
-router.get("/article/:id", (req, res) => {
+router.get('/article/:id', (req, res) => {
   let SQL = `select * from articles inner join articlesforcustmor on articles.id=articlesforcustmor.id WHERE articlesforcustmor.id=$1;`;
   client
     .query(SQL, [req.params.id])
-    .then((result) => {
-      res.render("pages/articles/article", {
+    .then(result => {
+      res.render('pages/articles/article', {
         oneArticleData: result.rows[0],
-        title: "Article",
+        title: 'Article',
       });
     })
-    .catch((err) => {
-      res.render("pages/error", { error: err });
+    .catch(err => {
+      res.render('pages/error', { error: err });
     });
 });
 
-router.get("/new-post", (req, res) => {
+router.get('/admin/new-post', (req, res) => {
   let SQL = `SELECT * FROM articles ORDER BY id DESC LIMIT 3;`;
-  client.query(SQL).then((result) => {
-    res.render("pages/articles/newPost", {
-      newArticlesAddes: result.rows,
-      title: "new Post",
+  client
+    .query(SQL)
+    .then(result => {
+      res.render('pages/articles/newPost', {
+        newArticlesAddes: result.rows,
+        title: 'new Post',
+      });
+    })
+    .catch(err => {
+      res.render('pages/error', { error: err });
     });
-  });
 });
 
-router.post("/new-post", (req, res) => {
+router.post('/admin/new-post', (req, res) => {
   let { image, title, author, description } = req.body;
-  // let date = new Date();
-  let date = new Intl.DateTimeFormat("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  }).format(new Date());
+  let { add } = req.body;
 
-  let SQL = `INSERT INTO articles (date ,image, title, author, description) VALUES ($1,$2,$3,$4,$5) RETURNING *; `;
+  let date = new Intl.DateTimeFormat('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }).format(new Date());
   let safeValues = [date, image, title, author, description];
 
-  client.query(SQL, safeValues).then((result) => {
-    res.redirect("/articles/new-post");
+  let SQL = add
+    ? client.query(`INSERT INTO articlesforcustmor (id) VALUES ($1);`, [add])
+    : client.query(
+        `INSERT INTO articles (date ,image, title, author, description) VALUES ($1,$2,$3,$4,$5) RETURNING *; `,
+        safeValues
+      );
+
+  SQL.then(result => {
+    res.redirect('/articles/admin/new-post');
+  }).catch(err => {
+    res.render('pages/error', { error: err });
   });
 });
 
-router.put("/new-post", (req, res) => {
+router.put('/admin/new-post', (req, res) => {
   let { image, date, title, author, description, id } = req.body;
   let SQL = `UPDATE articles SET image=$1, date=$2, title=$3, author=$4, description=$5 WHERE id=$6;`;
   let safeValues = [image, date, title, author, description, id];
 
   client
     .query(SQL, safeValues)
-    .then(res.redirect("/articles/new-post"))
-    .catch((err) => {
-      res.render("pages/error", { error: err });
+    .then(res.redirect('/articles/admin/new-post'))
+    .catch(err => {
+      res.render('pages/error', { error: err });
     });
 });
 
-router.delete("/new-post", (req, res) => {
+router.delete('/admin/new-post', (req, res) => {
   let SQL = `DELETE FROM articles WHERE id =$1;`;
   let safeValues = [req.body.deleteN];
-  client.query(SQL, safeValues).then(res.redirect("/articles//new-post"));
+  client
+    .query(SQL, safeValues)
+    .then(res.redirect('/articles/admin/new-post'))
+    .catch(err => {
+      res.render('pages/error', { error: err });
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-"use strict";
+'use strict';
 
 // Application Dependencies
-require("dotenv").config();
-const express = require("express");
-const superagent = require("superagent");
-const pg = require("pg");
-const cors = require("cors");
-const methodOverride = require("method-override");
+require('dotenv').config();
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const cors = require('cors');
+const methodOverride = require('method-override');
 
 // Application Setups
 const PORT = process.env.PORT || 3030;
 const server = express();
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  // ssl: { rejectUnauthorized: false },
 });
 server.use(express.urlencoded({ extended: true }));
-server.set("view engine", "ejs");
-server.use(express.static("./public"));
-server.use(methodOverride("_method"));
+server.set('view engine', 'ejs');
+server.use(express.static('./public'));
+server.use(methodOverride('_method'));
 server.use(cors());
 module.exports = client;
 
 // Start you routes here
 
 // Articles route
-const articlesRoute = require("./routes/articles.js");
-server.use("/articles", articlesRoute);
+const articlesRoute = require('./routes/articles.js');
+server.use('/articles', articlesRoute);
 ///// Constructers
 function YouTube(items) {
   this.videoID = items.id.videoId;
@@ -55,31 +55,31 @@ function Covid(items) {
   this.deaths = items.deaths;
 }
 // results route ------- most IMPORTANT route ---- APIs here
-server.get("/results", (req, res) => {
+server.get('/results', (req, res) => {
   // console.log("sdf");
   let URLparams = req.query.search_query;
   // console.log(URLparams);
   function countryNameFromCity(URLparams) {
     let key = process.env.GET_COUNTRY_API;
     let url = `http://api.positionstack.com/v1/forward?access_key=${key}&query=${URLparams}`;
-    return superagent.get(url).then((countryData) => {
+    return superagent.get(url).then(countryData => {
       let gettedData = countryData.body.data[0].country;
       return gettedData;
     });
   }
-  let countryApi = countryNameFromCity(URLparams).then((results) => {
+  let countryApi = countryNameFromCity(URLparams).then(results => {
     let url = `https://restcountries.eu/rest/v2/name/${results}?fullText=true`;
-    return superagent.get(url).then((capitalData) => {
+    return superagent.get(url).then(capitalData => {
       let gettedData = capitalData.body;
       let newcapitalData = new CountryInfo(gettedData);
       return newcapitalData;
     });
   });
-  let covidApi = countryNameFromCity(URLparams).then((results) => {
+  let covidApi = countryNameFromCity(URLparams).then(results => {
     let url = `https://covid-api.mmediagroup.fr/v1/cases?country=${
       results[0].toUpperCase() + results.slice(1)
     }`;
-    return superagent.get(url).then((covidData) => {
+    return superagent.get(url).then(covidData => {
       let Data = covidData.body.All;
       let CovidObjects = new Covid(Data);
       return CovidObjects;
@@ -89,9 +89,9 @@ server.get("/results", (req, res) => {
     let key = process.env.YOUTUBE_API_KEY;
     let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=tourism+in+${URLparams}&type=video&videoCaption=any&videoDefinition=any&videoDimension=2d&videoDuration=medium&videoEmbeddable=true&videoType=any&key=${key}`;
 
-    return superagent.get(url).then((youTubeData) => {
+    return superagent.get(url).then(youTubeData => {
       let gettedData = youTubeData.body.items;
-      let threeVideos = gettedData.map((items) => {
+      let threeVideos = gettedData.map(items => {
         return new YouTube(items);
       });
       return threeVideos;
@@ -100,7 +100,7 @@ server.get("/results", (req, res) => {
   function weatherApi(URLparams) {
     let key = process.env.WEATHER_API_KEY;
     let url = `api.openweathermap.org/data/2.5/weather?q=${URLparams}&units=metric&appid=${key}`;
-    return superagent.get(url).then((wData) => {
+    return superagent.get(url).then(wData => {
       let gettedData = wData.body;
       // console.log(gettedData);
       let newweatherData = new Weather(gettedData);
@@ -111,7 +111,7 @@ server.get("/results", (req, res) => {
     let safeValues = [URLparams.toLowerCase()];
     let SQL = `SELECT * FROM reviews WHERE city=$1;`;
 
-    return client.query(SQL, safeValues).then((results) => {
+    return client.query(SQL, safeValues).then(results => {
       // console.log(results.rows);
       // res.render("pages/results", { title: "sds", DBresults: results.rows });
       return results.rows;
@@ -124,10 +124,10 @@ server.get("/results", (req, res) => {
     covidApi,
     getRevies(URLparams),
   ])
-    .then((results) => {
+    .then(results => {
       console.log(results[4]);
-      res.render("pages/results", {
-        title: "results",
+      res.render('pages/results', {
+        title: 'results',
         youTubeApi: results[0],
         weatherApi: results[1],
         countryApi: results[2],
@@ -136,7 +136,7 @@ server.get("/results", (req, res) => {
         city: URLparams,
       });
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error);
       res.send(error);
       // res.render("./pages/error", { error: error });
@@ -144,7 +144,7 @@ server.get("/results", (req, res) => {
 });
 
 // To insert reviews into the DB
-server.post("/results", (req, res) => {
+server.post('/results', (req, res) => {
   // let URLparams = [req.params.search_query];
   console.log(req.body);
   let SQL = `INSERT INTO reviews (city, date, title, author, description,rating) VALUES($1,$2,$3,$4,$5,$6); `;
@@ -156,23 +156,23 @@ server.post("/results", (req, res) => {
     req.body.description,
     req.body.rating,
   ];
-  client.query(SQL, safeValues).then((results) => {
+  client.query(SQL, safeValues).then(results => {
     res.redirect(`/results/?search_query=${req.body.city}`);
   });
 });
 
 // AboutUs route
-server.get("/about-us", (req, res) => {
-  res.render("pages/about-us", { title: "About-Us" });
+server.get('/about-us', (req, res) => {
+  res.render('pages/about-us', { title: 'About-Us' });
 });
 // HomePage route
-server.get("/", (req, res) => {
-  res.render("pages/index", { title: "HomePage" });
+server.get('/', (req, res) => {
+  res.render('pages/index', { title: 'HomePage' });
 });
 
 // For any route that is not specified
-server.get("*", (req, res) => {
-  res.render("pages/error", { title: "Error" });
+server.get('*', (req, res) => {
+  res.render('pages/error', { title: 'Error' });
 });
 
 // Connect to dataBase then listen to the PORT
