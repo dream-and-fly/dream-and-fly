9cd445b65bc9d9258ab372ccfccb69c8b5c98031
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-05-18 04:50:46 +0300

    Finish everything

--- DAFtables.sql
+++ DAFtables.sql
@@ -35,13 +35,13 @@ CREATE TABLE users (
 
 
 INSERT INTO reviews (city, date, title, author, description,rating) 
-VALUES('amman','14/5/2021', 'Amman is a beutiful city', 'Abdullah', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using , making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).','2');
+VALUES('amman','14/5/2021', 'Amman is a beutiful city', 'Abdullah', 'I liked this city so much that I will come next time.','5');
 INSERT INTO reviews (city, date, title, author, description,rating) 
-VALUES('amman','14/5/2021', 'Amman is a beutiful city', 'Abdullah', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using , making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).','3');
+VALUES('amman','14/5/2021', 'Best city in the world', 'Amarah', 'I like it so much, it is kinda beautiful, I will come next time yo yo yo','5');
 INSERT INTO reviews (city, date, title, author, description,rating) 
-VALUES('amman','14/5/2021', 'Amman is a beutiful city', 'Abdullah', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using , making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).','5');
+VALUES('amman','14/5/2021', 'Nice city', 'M.Nimrawi', 'I like it so much, but I feel Irbid is better.','4');
 INSERT INTO reviews (city, date, title, author, description,rating) 
-VALUES('oslo','14/5/2021', 'Oslo is a beutiful city', 'Abo Ibrahim', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using , making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).','1');
+VALUES('oslo','14/5/2021', 'Oslo is a beutiful city', 'Abo Ibrahim', 'I like it so much, it is kinda beautiful, I will come next time yo yo yo.','1');
 
 
 INSERT INTO articlesforcustmor (id) VALUES (1);
